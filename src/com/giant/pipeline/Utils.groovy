713b53d197983fd9b83696a23e807e4365b686ef
lvelvis <421220622@qq.com>
2020-04-10 10:28:32 +0800

    add giant.pipeline

---
+++ src/com/giant/pipeline/Utils.groovy
@@ -0,0 +1,8 @@
+package com.giant.pipeline
+
+// 实现 Serializable 接口是为了确保当 pipeline 被 Jenkins挂起后能正确恢复
+class Utils implements Serializable {
+   def createVersion(String BUILD_NUMBER) {
+       return new Date().format('yyyyMM') + "-${BUILD_NUMBER}"
+   }
+}
